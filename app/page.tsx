56385a5eb6fe7c958ff9378a5c629a3606aa25ca
Sonny Lazuardi <sonnylazuardi@gmail.com>
2024-02-14 01:06:59 +0800

    Add shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import Image from "next/image";
+import Button from "@/components/ui/button";
 
 export default function Home() {
+  const handleClick = () => {
+    console.log("CLICK");
+  };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -26,6 +32,9 @@ export default function Home() {
             />
           </a>
         </div>
+        <Button variant="outline" onClick={handleClick}>
+          Click me
+        </Button>
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
