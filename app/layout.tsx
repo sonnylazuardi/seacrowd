f59228439b93661ab913855b1bd2a96de73b2368
Sonny Lazuardi <sonnylazuardi@gmail.com>
2024-02-16 23:33:39 +0800

    Add detail page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,12 +24,9 @@ export default function RootLayout({
           content="width=device-width, initial-scale=1, maximum-scale=1"
         ></meta>
       </Head>
-      <body className={inter.className}>{children}</body>
-      <script
-        async
-        src="https://eu.umami.is/script.js"
-        data-website-id="d770d165-8584-47dd-a009-af28af370ed6"
-      ></script>
+      <TooltipProvider>
+        <body className={inter.className}>{children}</body>
+      </TooltipProvider>
     </html>
   );
 }
